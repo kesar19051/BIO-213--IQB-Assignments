b1b7562b28ebd5df0908c2e5b8cf58718de4dcf1
Kesar Shrivastava <kesar19051@iiitd.ac.in>
2021-02-11 14:12:28 +0530

    multiple opi alignment left

--- Assignment 1/a.py
+++ Assignment 1/a.py
@@ -7,13 +7,18 @@ numOfColumns = len(x)+1 #6
 matrix = [[0 for i in range(numOfColumns)] for j in range(numOfRows)]
 matrixLocal = [[0 for i in range(numOfColumns)] for j in range(numOfRows)]
 
+mat = [[0 for i in range(numOfColumns)] for j in range(numOfRows)]
+matLocal = [[0 for i in range(numOfColumns)] for j in range(numOfRows)]
+
 matrix[0][0] = 0
 
 for i in range(1, numOfColumns):
     matrix[0][i] = matrix[0][i-1]-1
+    mat[0][i] = mat[0][i-1]-2
 
 for i in range(1, numOfRows):
     matrix[i][0] = matrix[i-1][0]-1
+    mat[i][0] = mat[i-1][0]-2
 
 for i in range(1, numOfRows):
     for j in range(1, numOfColumns):
@@ -24,15 +29,47 @@ for i in range(1, numOfRows):
             matrix[i][j] = max(max(matrix[i-1][j]-1, matrix[i][j-1]-1), matrix[i-1][j-1]-1)
             matrixLocal[i][j] = max(matrix[i][j], 0)
 
+for i in range(1, numOfRows):
+    for j in range(1, numOfColumns):
+        if y[i-1]==x[j-1]:
+            mat[i][j] = max(max(mat[i-1][j]-2, mat[i][j-1]-2), mat[i-1][j-1]+2)
+            matLocal[i][j] = max(mat[i][j], 0)
+        else:
+            mat[i][j] = max(max(mat[i-1][j]-2, mat[i][j-1]-2), mat[i-1][j-1]-1)
+            matLocal[i][j] = max(mat[i][j], 0)
+
+print("The global alignment matrix: ")
 for i in range(numOfRows):
     for j in range(numOfColumns):
         print(matrix[i][j], end = " ")
     print()
 
+print()
+
+print("The local alignment matrix: ")
 for i in range(numOfRows):
     for j in range(numOfColumns):
         print(matrixLocal[i][j], end = " ")
     print()
+
+print()
+
+print("The global alignment matrix with changed scores: ")
+for i in range(numOfRows):
+    for j in range(numOfColumns):
+        print(mat[i][j], end = " ")
+    print()
+
+print()
+
+print("The local alignment matrix with changed scores: ")
+for i in range(numOfRows):
+    for j in range(numOfColumns):
+        print(matLocal[i][j], end = " ")
+    print()
+
+print()
+
 
 xpos = numOfRows+numOfColumns-2
 ypos = numOfRows+numOfColumns-2
