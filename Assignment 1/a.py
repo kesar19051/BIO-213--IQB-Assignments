51b1206790378f0ff9d28d114011abf3c2e127e1
Kesar Shrivastava <kesar19051@iiitd.ac.in>
2021-02-13 13:25:14 +0530

    almost done

--- Assignment 1/a.py
+++ Assignment 1/a.py
@@ -4,6 +4,10 @@ y = "TTCAGTA"
 numOfRows = len(y)+1 #5 
 numOfColumns = len(x)+1 #6
 
+maxInMatrixLocal = -1
+indexI = 0
+indexJ = 0
+
 matrix = [[0 for i in range(numOfColumns)] for j in range(numOfRows)]
 matrixLocal = [[0 for i in range(numOfColumns)] for j in range(numOfRows)]
 
@@ -28,6 +32,10 @@ for i in range(1, numOfRows):
         else:
             matrix[i][j] = max(max(matrix[i-1][j]-1, matrix[i][j-1]-1), matrix[i-1][j-1]-1)
             matrixLocal[i][j] = max(max(max(matrixLocal[i-1][j]-1, matrixLocal[i][j-1]-1), matrixLocal[i-1][j-1]-1), 0)
+        if maxInMatrixLocal<=matrixLocal[i][j]:
+            maxInMatrixLocal = matrixLocal[i][j]
+            indexI = i
+            indexJ = j
 
 for i in range(1, numOfRows):
     for j in range(1, numOfColumns):
@@ -70,36 +78,86 @@ for i in range(numOfRows):
 
 print()
 
-
-xpos = numOfRows+numOfColumns-2
-ypos = numOfRows+numOfColumns-2
-
 i = numOfRows-1
 j = numOfColumns-1
 
-s1 = ["" for x in range(numOfRows+numOfColumns+1)]
-s2 = ["" for x in range(numOfRows+numOfColumns+1)]
+string1 = ""
+string2 = ""
+list = []
 
-while(i!=0 or j!=0):
+def func(s1, s2, i, j, array):
+    if(i==0 and j==0):
+        if s1 in list:
+            u = 1
+        else:
+            print(s1)
+            print(s2)
+            list.append(s1)
+            print()
+        return
     if y[i-1]==x[j-1]:
-        s1[xpos] = y[i-1]
-        s2[ypos] = x[j-1]
+        s1 = y[i-1]+s1
+        s2 = x[j-1]+s2
         i-=1
         j-=1
-    elif matrix[i-1][j-1]-1==matrix[i][j]:
-        s1[xpos] = y[i-1]
-        s2[ypos] = x[j-1]
+        func(s1, s2, i, j, array)
+    if matrix[i-1][j-1]-1==matrix[i][j]:
+        s1 = y[i-1]+s1
+        s2 = x[j-1]+s2
         i-=1
         j-=1
-    elif matrix[i-1][j]-1==matrix[i][j]:
-        s1[xpos] = y[i-1]
-        s2[ypos] = "_"
+        func(s1, s2, i, j, array)
+    if matrix[i-1][j]-1==matrix[i][j]:
+        s1 = y[i-1] + s1
+        s2 = "_" + s2
         i-=1
-    elif matrix[i][j-1]-1==matrix[i][j]:
-        s1[xpos] = "_"
-        s2[ypos] = x[j-1]
+        func(s1, s2, i, j, array)
+    if matrix[i][j-1]-1==matrix[i][j]:
+        s1 = "_" + s1
+        s2 = x[j-1] + s2
         j-=1
-    xpos-=1
-    ypos-=1
-print(s1)
-print(s2)
+        func(s1, s2, i, j, array)
+
+func(string1, string2, i, j, matrix)
+
+string1 = ""
+string2 = ""
+list = []
+
+print("----------LOCAL-----------")
+
+def localFunc(s1, s2, i, j, array):
+    if(array[i][j]==0):
+        if s1 in list:
+            u = 1
+        else:
+            print(s1)
+            print(s2)
+            list.append(s1)
+            print()
+        return
+    if y[i-1]==x[j-1]:
+        s1 = y[i-1]+s1
+        s2 = x[j-1]+s2
+        i-=1
+        j-=1
+        localFunc(s1, s2, i, j, array)
+    if matrix[i-1][j-1]-1==matrix[i][j]:
+        s1 = y[i-1]+s1
+        s2 = x[j-1]+s2
+        i-=1
+        j-=1
+        localFunc(s1, s2, i, j, array)
+    if matrix[i-1][j]-1==matrix[i][j]:
+        s1 = y[i-1] + s1
+        s2 = "_" + s2
+        i-=1
+        localFunc(s1, s2, i, j, array)
+    if matrix[i][j-1]-1==matrix[i][j]:
+        s1 = "_" + s1
+        s2 = x[j-1] + s2
+        j-=1
+        localFunc(s1, s2, i, j, array)
+
+localFunc(string1, string2, indexI, indexJ, matLocal)
+
