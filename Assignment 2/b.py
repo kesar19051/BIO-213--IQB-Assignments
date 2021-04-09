2dbb918d106d9f129a4bb40dcf388aa38c03dbfc
Kesar Shrivastava <kesar19051@iiitd.ac.in>
2021-04-10 00:55:42 +0530

    alpha helix done

--- Assignment 2/b.py
+++ Assignment 2/b.py
@@ -7,3 +7,51 @@ p_beta = {'A': 0.97, 'R': 0.90, 'N': 0.65, 'D': 0.80, 'C': 1.30, 'E': 0.26, 'Q':
 
 helix = {'E': 1, 'A': 1, 'L': 1, 'H': 1, 'M': 1, 'Q': 1, 'W': 1, 'V': 1, 'F': 1, 'K': 0.5, 'I': 0.5, 'D': 0, 'T': 0, 'S': 0, 'R': 0, 'C': 0, 'N': -1, 'Y': -1, 'P': -1, 'G': -1}
 sheet = {'M': 1, 'V': 1, 'I': 1, 'C': 1, 'Y': 1, 'F': 1, 'Q': 1, 'L': 1, 'T': 1, 'W': 1, 'A': 0.5, 'R': 0, 'G': 0, 'D': 0, 'K': -1, 'S': -1, 'H': -1, 'N': -1,'P': -1, 'E': -1}
+
+listHelix = []
+listSheet = []
+#this string will contain if a residue is identified to be a part of alpha helix or beta sheet
+ans = "."*len(s)
+
+#identifying alpha helix
+for i in range(len(s)-5):
+	window = s[i:i+6]
+	score = 0
+	for x in window:
+		score = score + helix.get(x)
+	if score>=4:
+		for j in range(6):
+			if i+j in listHelix:
+				continue
+			else:
+				listHelix.append(i+j)
+		extensionScore = 0
+		ex = i+6
+		if ex+3<len(s):
+				extensionWindow = s[ex:ex+4]
+				for y in extensionWindow:
+					extensionScore = extensionScore + p_alpha.get(y)
+				ex = ex+4
+		while extensionScore>=4 and ex+3<len(s):
+			extensionWindow = s[ex:ex+4]
+			extensionScore = 0
+			for y in extensionWindow:
+				extensionScore = extensionScore + p_alpha.get(y)
+			ex = ex+4
+		r = ex-4-i-6
+		for j in range(r):
+			if i+j+6 in listHelix:
+				continue
+			else:
+				listHelix.append(i+j+6)
+
+for x in range(len(listHelix)):
+    print (listHelix[x], end = " ")
+
+			
+
+
+
+
+
+
